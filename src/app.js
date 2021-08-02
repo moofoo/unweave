8929e54aab0922767546a153ebe7022aca5018e2
Nathan Cook <nathan@trumidtech.com>
2021-08-02 16:02:06 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@ const { debugSession } = require('./debugsession.js');
 const { init, connectToInspector, enableDebugger, makeInspectorUri, startDebugSession, startDebugSession2, startInspectedProcess } = require('./init.js');
 const protocol = require('./protocol.js');
 const components = require('./components.js');
+const { makeEmitter, mergeEvents, later, Source } = require('@acransac/streamer');
 
 module.exports = {
   debugSession,
   init, connectToInspector, enableDebugger, makeInspectorUri, startDebugSession2, startInspectedProcess,
   protocol,
-  components
+  components,
+  makeEmitter,
+  mergeEvents,
+  later,
+  Source
 }
 
 // init(process.argv, debugSession, () => {});
