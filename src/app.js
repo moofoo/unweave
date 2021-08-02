10e1c8d3c90fbdb45b9e48cd3f1eea45ca049ccd
Nathan Cook <nathan@trumidtech.com>
2021-08-02 15:56:22 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@
 const { debugSession } = require('./debugsession.js');
 const { init, connectToInspector, enableDebugger, makeInspectorUri, startDebugSession, startDebugSession2, startInspectedProcess } = require('./init.js');
 const protocol = require('./protocol.js');
+const components = require('./components.js');
 
 module.exports = {
   debugSession,
   init, connectToInspector, enableDebugger, makeInspectorUri, startDebugSession2, startInspectedProcess,
-  protocol
+  protocol,
+  components
 }
 
 // init(process.argv, debugSession, () => {});
